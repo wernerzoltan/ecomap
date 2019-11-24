85a6b8beb7fb7589a64467133a753972346588ed
Zoltán Werner <39615472+wernerzoltan@users.noreply.github.com>
2019-11-24 20:27:53 +0100

    Initial commit

---
+++ src/test/java/ecomap/ecomap/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ecomap.ecomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
